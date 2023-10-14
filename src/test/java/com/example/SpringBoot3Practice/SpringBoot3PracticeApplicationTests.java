6ad66d13cb385427852d89df0c567ec4dabb2efb
Imasa <imiplayacc@gmail.com>
2023-10-15 07:14:12 +0900

    initial commit.

---
+++ src/test/java/com/example/SpringBoot3Practice/SpringBoot3PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot3Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
